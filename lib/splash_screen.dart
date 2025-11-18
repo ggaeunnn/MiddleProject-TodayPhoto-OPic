df03fa5a61a10f55423212ec0a9737cca1f4bf46
Shiny Lee <shinish512@gmail.com>
2025-11-18 16:01:23 +0900

    스플래시 수정(길이 조절)

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -18,7 +18,7 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   Future<void> _initialize() async {
-    await Future.delayed(const Duration(seconds: 5));
+    await Future.delayed(const Duration(seconds: 4));
 
     if (mounted) {
       context.go('/home');
